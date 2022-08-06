a456461f64610d21fadc904317a6ab7365cdde53
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-08-06 19:57:13 +0000

    Fix linter error

--- solver.rb
+++ solver.rb
@@ -1,35 +1,29 @@
 class Solver
-  
-    def factorial(n)
-
-        if(n < 0)
-          throw "Negative value not allowed"
-        end
-        
-        if(n == 1) 
-            return 1
-        end
-
-        return n * factorial(n - 1)
+  def factorial(n)
+    if (n < 0)
+      throw "Negative value not allowed"
     end
 
-    def reverse(word)
-        return word.reverse
+    if (n == 1)
+      return 1
     end
 
-    def fizzbuzz(n)
-        if n % 3 == 0 and n % 5 == 0
-            return "fizzbuzz"
-        elsif n % 3 == 0
-            return "fizz"
-        elsif n % 5 == 0
-            return "buzz"
-        else
-            return n
-        end
-    end
+    return n * factorial(n - 1)
+  end
 
+  def reverse(word)
+    return word.reverse
+  end
+
+  def fizzbuzz(n)
+    if (n % 3).zero? and (n % 5).zero?
+      "fizzbuzz"
+    elsif (n % 3).zero?
+      "fizz"
+    elsif (n % 5).zero?
+      "buzz"
+    else
+      n
+    end
+  end
 end
-
-solver = Solver.new
-print solver.fizzbuzz(7);
